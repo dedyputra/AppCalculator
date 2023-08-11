43a55c8e58da0103d454613363d1acdbbd8066dc
Dedy Putra Aditia <73837147+dedyputra@users.noreply.github.com>
2023-08-11 22:12:06 +0700

    init semangat bolo

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('My Calculatorku :)'),
+          title: const Text('My calculator ku :)'),
         ),
         body: const Padding(
           padding: EdgeInsets.all(18.0),
